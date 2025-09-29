fc893310dc739ba5378e6285924d477ed467eefc
gravastar212 <gravastar1992212@gmail.com>
2025-09-29 15:50:11 +0200

    chore: setup environment variables (.env + .env.example)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
